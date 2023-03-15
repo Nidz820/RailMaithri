6b575f7393eb5a20ebb281dea7f0d5c58304ebc5
Thinkal V B <thinkalvb@gmail.com>
2023-03-15 14:18:50 +0530

    Login page design

--- app/src/main/java/org/keltron/railmaithri/Login.kt
+++ app/src/main/java/org/keltron/railmaithri/Login.kt
@@ -2,10 +2,27 @@ package org.keltron.railmaithri
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 
 class Login : AppCompatActivity() {
+    lateinit var progressPB:    ProgressBar
+    lateinit var usernameET:    EditText
+    lateinit var passwordET:    EditText
+    lateinit var loginBT:       Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        supportActionBar!!.hide()
+
+        progressPB = findViewById(R.id.progressBar)
+        usernameET = findViewById(R.id.username)
+        passwordET = findViewById(R.id.password)
+        loginBT    = findViewById(R.id.login)
+
+        progressPB.visibility = View.GONE
     }
 }
