2f991d2cfd1dbbe94984d49b342c5f2475f69969
Thinkal V B <thinkalvb@gmail.com>
2023-03-16 16:01:09 +0530

    Created incident report activity

--- app/src/main/java/org/keltron/railmaithri/IncidentReport.kt
+++ app/src/main/java/org/keltron/railmaithri/IncidentReport.kt
@@ -3,10 +3,9 @@ package org.keltron.railmaithri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Home : AppCompatActivity() {
+class IncidentReport : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home)
-        supportActionBar!!.hide()
+        setContentView(R.layout.incident_report)
     }
 }
