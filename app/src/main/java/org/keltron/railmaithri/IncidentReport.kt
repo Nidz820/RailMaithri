ce72cdb534a9d9df3ae8c9f32b4568b6ca285cae
Thinkal V B <thinkalvb@gmail.com>
2023-03-17 14:07:13 +0530

    Created incident report page

--- app/src/main/java/org/keltron/railmaithri/IncidentReport.kt
+++ app/src/main/java/org/keltron/railmaithri/IncidentReport.kt
@@ -7,5 +7,6 @@ class IncidentReport : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.incident_report)
+        supportActionBar!!.hide()
     }
 }
