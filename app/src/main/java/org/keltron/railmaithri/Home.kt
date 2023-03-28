774f92a799165595d6d1f2b11194c3475b449eb1
Thinkal V B <thinkalvb@gmail.com>
2023-03-28 16:03:01 +0530

    Home page design

--- app/src/main/java/org/keltron/railmaithri/Home.kt
+++ app/src/main/java/org/keltron/railmaithri/Home.kt
@@ -12,23 +12,23 @@ class Home : AppCompatActivity() {
         setContentView(R.layout.home)
         supportActionBar!!.hide()
 
-        val addIncidentBT    = findViewById<Button>(R.id.add_incident_report)
-        val searchIncidentBT = findViewById<Button>(R.id.search_incident_report)
-        val savedIncidentBT  = findViewById<Button>(R.id.saved_incident_report)
-        addIncidentBT.setOnClickListener {
-            val intent = Intent(this, IncidentReport::class.java)
-            intent.putExtra("mode", Scope.MODE_NEW_FORM)
-            startActivity(intent)
-        }
-        savedIncidentBT.setOnClickListener {
-            val intent = Intent(this, SavedData::class.java)
-            intent.putExtra("scope", Scope.INCIDENT_REPORT)
-            startActivity(intent)
-        }
-        searchIncidentBT.setOnClickListener {
-            val intent = Intent(this, SearchData::class.java)
-            intent.putExtra("scope", Scope.INCIDENT_REPORT)
-            startActivity(intent)
-        }
+//        val addIncidentBT    = findViewById<Button>(R.id.add_incident_report)
+//        val searchIncidentBT = findViewById<Button>(R.id.search_incident_report)
+//        val savedIncidentBT  = findViewById<Button>(R.id.saved_incident_report)
+//        addIncidentBT.setOnClickListener {
+//            val intent = Intent(this, IncidentReport::class.java)
+//            intent.putExtra("mode", Scope.MODE_NEW_FORM)
+//            startActivity(intent)
+//        }
+//        savedIncidentBT.setOnClickListener {
+//            val intent = Intent(this, SavedData::class.java)
+//            intent.putExtra("scope", Scope.INCIDENT_REPORT)
+//            startActivity(intent)
+//        }
+//        searchIncidentBT.setOnClickListener {
+//            val intent = Intent(this, SearchData::class.java)
+//            intent.putExtra("scope", Scope.INCIDENT_REPORT)
+//            startActivity(intent)
+//        }
     }
 }
