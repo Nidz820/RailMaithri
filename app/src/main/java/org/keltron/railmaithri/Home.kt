5a99750dba84ff16466367756a890052cd7a102b
Thinkal V B <thinkalvb@gmail.com>
2023-03-29 11:18:51 +0530

    Home page design completed

--- app/src/main/java/org/keltron/railmaithri/Home.kt
+++ app/src/main/java/org/keltron/railmaithri/Home.kt
@@ -2,8 +2,10 @@ package org.keltron.railmaithri
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
 
 
 class Home : AppCompatActivity() {
@@ -12,23 +14,16 @@ class Home : AppCompatActivity() {
         setContentView(R.layout.home)
         supportActionBar!!.hide()
 
-//        val addIncidentBT    = findViewById<Button>(R.id.add_incident_report)
-//        val searchIncidentBT = findViewById<Button>(R.id.search_incident_report)
-//        val savedIncidentBT  = findViewById<Button>(R.id.saved_incident_report)
-//        addIncidentBT.setOnClickListener {
-//            val intent = Intent(this, IncidentReport::class.java)
-//            intent.putExtra("mode", Scope.MODE_NEW_FORM)
-//            startActivity(intent)
-//        }
-//        savedIncidentBT.setOnClickListener {
-//            val intent = Intent(this, SavedData::class.java)
-//            intent.putExtra("scope", Scope.INCIDENT_REPORT)
-//            startActivity(intent)
-//        }
-//        searchIncidentBT.setOnClickListener {
-//            val intent = Intent(this, SearchData::class.java)
-//            intent.putExtra("scope", Scope.INCIDENT_REPORT)
-//            startActivity(intent)
-//        }
+        val profile  = JSONObject(Helper.getData(this, Scope.PROFILE)!!)
+        val username = profile.getString("username")
+        findViewById<TextView>(R.id.profile_name).text = username
+
+        val logoutButton = findViewById<ImageButton>(R.id.logout)
+        logoutButton.setOnClickListener {
+            Helper.saveData(this, Scope.TOKEN, "")
+            startActivity(Intent(this, Login::class.java))
+            finish()
+        }
+
     }
 }
